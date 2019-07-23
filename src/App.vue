b0a2d36a5e4be5ae7ce34bb10d2c0a4d6e226104
Alena <elen.malets@gmail.com>
2019-07-23 12:17:20 +0200

    allQuotes and Quote components

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+        .container
+            allquotes(:quotes="quotes")
+</template>
+
+<script>
+    import AllQuotes from './components/AllQuotes.vue'
+
+    export default {
+        data() {
+            return {
+                maxQuotes: 10,
+                quotes: ['my first quote'],
+            }
+        },
+        components: {
+            allquotes: AllQuotes,
+
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/AllQuotes.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    .row 
+        quote(v-for='quote in quotes') {{quote}} 
+</template>
+
+
+<script>
+import Quote from './Quote.vue'
+export default {
+    props: ['quotes'],
+    components: {
+        quote: Quote,
+    }
+}
+</script>
+
+<style>
+
+</style>
